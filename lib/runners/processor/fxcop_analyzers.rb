c7575480b7869415711ff87c6d38e3523e013db5
Gaku Nakagawa <gaku@sleeek.io>
2020-02-13 07:06:11 +0900

    implement runner (normal case only)

---
+++ lib/runners/processor/fxcop_analyzers.rb
@@ -0,0 +1,113 @@
+module Runners
+  # A processor class for FxCop Analyzers
+  # https://docs.microsoft.com/en-us/visualstudio/code-quality/install-fxcop-analyzers
+  # This is a beta release. We support only repositories that has .csproj file directly under analysis root directory.
+  class Processor::FxCopAnalyzers < Processor
+
+    #parameters
+    FXCOP_ANALYZER_VERSION = '2.9.8'.freeze
+    ANALYSIS_LOGFILE_PATH = '/tmp/sider_analysis_out.json'.freeze
+    RULE_ID_PATTERN = /CA[0-9]+/
+
+    def self.ci_config_section_name
+      'fxcop_analyzers'
+    end
+
+    def analyzer_name
+      'FxCop Analyzers'
+    end
+
+    def analyzer_bin
+      'dotnet'
+    end
+
+    def analyzer_version
+      FXCOP_ANALYZER_VERSION
+    end
+
+    def setup
+      # restore project (install dependencies)
+      cmdline_install_dependency = %w(dotnet restore)
+      _, _, status = capture3(*cmdline_install_dependency)
+      unless status == 0
+        msg = <<~EOS
+        Failed to restore .NET Core Project. You have to put project file (.csproj) under the analysis root. Please check project structure and Sider config file (sider.yml).
+        Note: We only support a project managed with MSBuild. No support for other build systems. (e.g. Nuke, Cake)
+        EOS
+        return Results::Failure.new(guid: guid, message: msg, analyzer: analyzer)
+      end
+
+      # install FxCop Analyzers from nuget Repository
+      cmdline_add_analyzer = ['dotnet', 'add', 'package', 'Microsoft.CodeAnalysis.FxCopAnalyzers', '--version', FXCOP_ANALYZER_VERSION]
+      _, _, status = capture3(*cmdline_add_analyzer)
+      unless status == 0
+        msg = <<~EOS
+        Failed to install FxCop Analyzers. Please check log output.
+        EOS
+        return Results::Failure.new(guid: guid, message: msg, analyzer: analyzer)
+      end
+
+      yield
+    end
+
+    def analyze(changes)
+      # run 'dotnet build' with static analysis module
+      cmdline_run_analyzer = ['dotnet', 'build', '--no-incremental', "-property:errorlog=#{ANALYSIS_LOGFILE_PATH}"]
+      capture3!(*cmdline_run_analyzer)
+
+      # parse static analysis results
+      unless File.exist?(ANALYSIS_LOGFILE_PATH)
+        return Results::Failure.new(guid: guid, message: 'Static analysis result is missing.', analyzer: analyzer)
+      end
+      issues = parse_result_file(File.open("#{ANALYSIS_LOGFILE_PATH}"))
+
+      # generate a result instance
+      Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
+        issues.each do |issue|
+          loc = Location.new(
+            start_line: issue[:start_line],
+            start_column: issue[:start_column],
+            end_line: issue[:end_line],
+            end_column: issue[:end_column]
+          )
+          result.add_issue Issue.new(
+            path: relative_path(issue[:file]),
+            location: loc,
+            id: issue[:rule_id],
+            message: issue[:message])
+        end
+      end
+    end
+
+    # parse error log file(static analysis log file) from .NET Core Compilers
+    def parse_result_file(f)
+      rval = []
+        json = JSON.parse(f.read)
+        json.fetch('runs').each do |i|
+          i.fetch('results').each do |i2|
+            rule_id  = i2.fetch('ruleId')
+            message  = i2.fetch('message')
+            loc_info = i2.fetch('locations').fetch(0).fetch('resultFile').fetch('region')
+            file     = i2.fetch('locations').fetch(0).fetch('resultFile').fetch('uri').sub(/^file:\/\//,'')
+
+            # skip issues if the rule id is NOT for FxCop Analyzers
+            unless rule_id =~ RULE_ID_PATTERN
+              continue
+            end
+            rval.append(
+              {
+                "file": file,
+                "rule_id": rule_id,
+                "message": message,
+                "start_line": loc_info.fetch('startLine'),
+                "start_column": loc_info.fetch('startColumn'),
+                "end_line": loc_info.fetch('endLine'),
+                "end_column": loc_info.fetch('endColumn')
+              }
+            )
+          end
+        end
+      rval
+    end
+  end
+end
---
+++ test/smokes/fxcop_analyzers/expectations.rb
@@ -0,0 +1,56 @@
+Smoke = Runners::Testing::Smoke
+
+# a normal case
+Smoke.add_test(
+  'success',
+  {
+    guid: 'test-guid',
+    timestamp: :_,
+    type: 'success',
+    issues: [
+      {
+        path: 'Program.cs',
+        location: { start_line: 9, start_column: 31, end_line:9, end_column:45 },
+        id: 'CA1303',
+        message: "Method 'void Program.Main(string[] args)' passes a literal string as parameter 'value' of a call to 'void Console.WriteLine(string value)'. Retrieve the following string(s) from a resource table instead: \"Hello World!\".",
+        object: nil,
+        git_blame_info: nil,
+        links: []
+      },
+      {
+        path: 'Program.cs',
+        location: { start_line: 7, start_column: 35, end_line:7, end_column:39 },
+        id: 'CA1801',
+        message: 'Parameter args of method Main is never used. Remove the parameter or use it in the method body.',
+        object: nil,
+        git_blame_info: nil,
+        links: []
+      }
+    ],
+    analyzer: { name: 'FxCop Analyzers', version: '2.9.8' }
+  }
+)
+
+# a project don't have .NET Core Project file (csproj)
+Smoke.add_test(
+  'no_csproj',
+  {
+    guid: 'test-guid',
+    timestamp: :_,
+    type: 'failure',
+    message: :_,
+    analyzer: :_,
+  }
+)
+
+# a project have invalid .NET Core Project file (csproj)
+Smoke.add_test(
+  'invalid_csproj',
+  {
+    guid: 'test-guid',
+    timestamp: :_,
+    type: 'failure',
+    message: :_,
+    analyzer: :_,
+  }
+)
