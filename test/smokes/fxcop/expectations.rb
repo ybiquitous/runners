58306a16b9f1a702eea990f074319be849508576
Gaku Nakagawa <gaku@sleeek.io>
2020-02-21 08:15:44 +0900

    add a smoke test case (.csproj file in non project root dir)

--- test/smokes/fxcop/expectations.rb
+++ test/smokes/fxcop/expectations.rb
@@ -105,6 +105,42 @@ Smoke.add_test(
   }
 )
 
+# a project have .csproj file in non analysis root
+Smoke.add_test(
+  'success_csproj_in_non_root_dir',
+  {
+    guid: 'test-guid',
+    timestamp: :_,
+    type: 'success',
+    issues: [
+      {
+        path: 'src/Program.cs',
+        location: { start_line: 9, start_column: 31, end_line:9, end_column:45 },
+        id: 'CA1303',
+        message: "Method 'void Program.Main(string[] args)' passes a literal string as parameter 'value' of a call to 'void Console.WriteLine(string value)'. Retrieve the following string(s) from a resource table instead: \"Hello World!\".",
+        object: {
+          severity: 'warning'
+        },
+        git_blame_info: nil,
+        links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1303-do-not-pass-literals-as-localized-parameters"]
+      },
+      {
+        path: 'src/Program.cs',
+        location: { start_line: 7, start_column: 35, end_line:7, end_column:39 },
+        id: 'CA1801',
+        message: 'Parameter args of method Main is never used. Remove the parameter or use it in the method body.',
+        object: {
+          severity: 'warning'
+        },
+        git_blame_info: nil,
+        links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      }
+    ],
+    analyzer: { name: 'fxcop', version: '2.9.8' }
+  }
+)
+
+
 # a project don't have .NET Core Project file (csproj)
 Smoke.add_test(
   'no_csproj',
@@ -126,5 +162,40 @@ Smoke.add_test(
     type: 'failure',
     message: :_,
     analyzer: :_,
+  }
+)
+
+# a project have .csproj file in non analysis root
+Smoke.add_test(
+  'success',
+  {
+    guid: 'test-guid',
+    timestamp: :_,
+    type: 'success',
+    issues: [
+      {
+        path: 'Program.cs',
+        location: { start_line: 9, start_column: 31, end_line:9, end_column:45 },
+        id: 'CA1303',
+        message: "Method 'void Program.Main(string[] args)' passes a literal string as parameter 'value' of a call to 'void Console.WriteLine(string value)'. Retrieve the following string(s) from a resource table instead: \"Hello World!\".",
+        object: {
+          severity: 'warning'
+        },
+        git_blame_info: nil,
+        links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1303-do-not-pass-literals-as-localized-parameters"]
+      },
+      {
+        path: 'Program.cs',
+        location: { start_line: 7, start_column: 35, end_line:7, end_column:39 },
+        id: 'CA1801',
+        message: 'Parameter args of method Main is never used. Remove the parameter or use it in the method body.',
+        object: {
+          severity: 'warning'
+        },
+        git_blame_info: nil,
+        links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      }
+    ],
+    analyzer: { name: 'fxcop', version: '2.9.8' }
   }
 )
