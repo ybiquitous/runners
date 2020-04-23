f63c89bf4460885bd2a114bfebe32b397de46e94
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 13:31:16 +0900

    Apply Prettier for FxCop smoke test code (#997)

--- test/smokes/fxcop/expectations.rb
+++ test/smokes/fxcop/expectations.rb
@@ -2,96 +2,102 @@ s = Runners::Testing::Smoke
 
 # a normal case
 s.add_test(
-  'success',
-  type: 'success',
+  "success",
+  type: "success",
   issues: [
     {
-      path: 'Program.cs',
+      path: "Program.cs",
       location: { start_line: 7, start_column: 35, end_line: 7, end_column: 39 },
       id: "CA1801",
       message: "Parameter args of method Main is never used. Remove the parameter or use it in the method body.",
       object: {
         category: "Usage",
-        description: "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
-        severity: "Warning",
+        description:
+          "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters]
     },
     {
-      path: 'bar/Hello.cs',
+      path: "bar/Hello.cs",
       location: { start_line: 7, start_column: 41, end_line: 7, end_column: 45 },
       id: "CA1801",
       message: "Parameter text of method Print is never used. Remove the parameter or use it in the method body.",
       object: {
         category: "Usage",
-        description: "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
-        severity: "Warning",
+        description:
+          "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters]
     },
     {
-      path: 'Program.cs',
+      path: "Program.cs",
       location: { start_line: 5, start_column: 11, end_line: 5, end_column: 18 },
       id: "CA1812",
-      message: "Program is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).",
+      message:
+        "Program is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).",
       object: {
         category: "Performance",
         description: "An instance of an assembly-level type is not created by code in the assembly.",
-        severity: "Warning",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1812-avoid-uninstantiated-internal-classes"]
-    },
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1812-avoid-uninstantiated-internal-classes]
+    }
   ],
-  analyzer: { name: 'FxCop', version: '2.9.8' }
+  analyzer: { name: "FxCop", version: "2.9.8" }
 )
 
 # a project have .csproj file in non analysis root
 s.add_test(
-  'success_csproj_in_non_root_dir',
-  type: 'success',
+  "success_csproj_in_non_root_dir",
+  type: "success",
   issues: [
     {
-      path: 'src/Program.cs',
+      path: "src/Program.cs",
       location: { start_line: 7, start_column: 35, end_line: 7, end_column: 39 },
       id: "CA1801",
       message: "Parameter args of method Main is never used. Remove the parameter or use it in the method body.",
       object: {
         category: "Usage",
-        description: "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
-        severity: "Warning",
+        description:
+          "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters]
     },
     {
-      path: 'src/bar/Hello.cs',
+      path: "src/bar/Hello.cs",
       location: { start_line: 7, start_column: 41, end_line: 7, end_column: 45 },
       id: "CA1801",
       message: "Parameter text of method Print is never used. Remove the parameter or use it in the method body.",
       object: {
         category: "Usage",
-        description: "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
-        severity: "Warning",
+        description:
+          "Avoid unused paramereters in your code. If the parameter cannot be removed, then change its name so it starts with an underscore and is optionally followed by an integer, such as '_', '_1', '_2', etc. These are treated as special discard symbol names.",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters"]
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1801-review-unused-parameters]
     },
     {
-      path: 'src/Program.cs',
+      path: "src/Program.cs",
       location: { start_line: 5, start_column: 11, end_line: 5, end_column: 18 },
       id: "CA1812",
-      message: "Program is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).",
+      message:
+        "Program is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).",
       object: {
         category: "Performance",
         description: "An instance of an assembly-level type is not created by code in the assembly.",
-        severity: "Warning",
+        severity: "Warning"
       },
       git_blame_info: nil,
-      links: ["https://docs.microsoft.com/visualstudio/code-quality/ca1812-avoid-uninstantiated-internal-classes"]
-    },
+      links: %w[https://docs.microsoft.com/visualstudio/code-quality/ca1812-avoid-uninstantiated-internal-classes]
+    }
   ],
-  analyzer: { name: 'FxCop', version: '2.9.8' }
+  analyzer: { name: "FxCop", version: "2.9.8" }
 )
