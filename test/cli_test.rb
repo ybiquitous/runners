d72c4cad03be2a4c25fe421942be9864fbf8f57e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-06-24 19:20:16 +0900

    Remove unstable `CLITest#test_processor_class` (#1227)
    Re-challenge for PR #1226.
    
    `CLITest#test_processor_class` is unstable (sometimes failed) but not useful to detect bugs.
    I think the current smoke tests are enough if `CLITest#test_processor_class` is missing.

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -138,19 +138,6 @@ class CLITest < Minitest::Test
     end
   end
 
-  def test_processor_class
-    with_runners_options_env(source: { head: 'http://example.com/head' }) do
-      cli = CLI.new(argv: %w[--analyzer=rubocop test-guid], stdout: stdout, stderr: stderr)
-      assert_equal Runners::Processor::RuboCop, cli.processor_class
-
-      cli = CLI.new(argv: %w[--analyzer=scss_lint test-guid], stdout: stdout, stderr: stderr)
-      assert_equal Runners::Processor::ScssLint, cli.processor_class
-
-      error = assert_raises { CLI.new(argv: %w[--analyzer=foo test-guid], stdout: stdout, stderr: stderr) }
-      assert_equal "Not found processor class with 'foo'", error.message
-    end
-  end
-
   def test_format_duration
     with_runners_options_env(source: { head: 'http://example.com/head' }) do
       cli = CLI.new(argv: %w[--analyzer=rubocop test-guid], stdout: stdout, stderr: stderr)
