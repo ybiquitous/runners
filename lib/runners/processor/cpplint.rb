0e31f8d120b3b84a93a53c224b05d8ebc2f00fa7
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-27 17:08:46 +0900

    Refactor `Runners::Processor::Cpplint` class (#1430)
    Extract from #1215

--- lib/runners/processor/cpplint.rb
+++ lib/runners/processor/cpplint.rb
@@ -1,7 +1,8 @@
 module Runners
   class Processor::Cpplint < Processor
-    Schema = StrongJSON.new do
-      let :runner_config, Schema::BaseConfig.base.update_fields { |fields|
+    Schema = _ = StrongJSON.new do
+      # @type self: SchemaClass
+      let :runner_config, Runners::Schema::BaseConfig.base.update_fields { |fields|
         fields.merge!({
           target: enum?(string, array(string)),
           extensions: string?,
@@ -22,7 +23,18 @@ module Runners
     DEFAULT_TARGET = ".".freeze
 
     def analyze(changes)
-      run_analyzer
+      _stdout, stderr, status = capture3 analyzer_bin, *analyzer_options
+
+      if [0, 1].include? status.exitstatus
+        xml_output = REXML::Document.new(stderr).root
+        if xml_output
+          Results::Success.new(guid: guid, analyzer: analyzer, issues: parse_result(xml_output))
+        else
+          Results::Failure.new(guid: guid, analyzer: analyzer, message: stderr)
+        end
+      else
+        Results::Failure.new(guid: guid, analyzer: analyzer, message: stderr)
+      end
     end
 
     private
@@ -40,25 +52,6 @@ module Runners
       end
     end
 
-    def run_analyzer
-      _stdout, stderr, status = capture3 analyzer_bin, *analyzer_options
-
-      if [0, 1].include? status.exitstatus
-        xml_output = REXML::Document.new(stderr)&.root
-        if xml_output
-          Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
-            parse_result(xml_output) do |issue|
-              result.add_issue issue
-            end
-          end
-        else
-          Results::Failure.new(guid: guid, analyzer: analyzer, message: stderr)
-        end
-      else
-        Results::Failure.new(guid: guid, analyzer: analyzer, message: stderr)
-      end
-    end
-
     # Output format:
     #
     #     {line}: {message} [{category}] [{confidence}]
@@ -69,33 +62,34 @@ module Runners
     #
     # @see https://github.com/cpplint/cpplint/blob/1.5.2/cpplint.py#L1396
     # @see https://github.com/cpplint/cpplint/blob/1.5.2/cpplint.py#L1686-L1693
-    def parse_result(xml_doc)
-      message_pattern = /([^:]+): (.+) \[(.+)\] \[(.+)\]/
+    def parse_result(xml_root)
+      issue_pattern = /^([^:]+): (.+) \[(.+)\] \[(.+)\]$/
+      issues = []
 
-      xml_doc.each_element("testcase") do |testcase|
-        path = relative_path(testcase[:name])
+      xml_root.each_element("testcase") do |testcase|
+        filename = testcase[:name] or raise "Required name: #{testcase.inspect}"
+        path = relative_path(filename)
+
         testcase.each_element("failure") do |failure|
-          failure.text.split("\n").map(&:strip).each do |issue_line|
-            matched = issue_line.match(message_pattern)
-            if matched
-              line, message, category, confidence = matched.captures
-              no_line_number = (line == "0" || !line.match?(/\A\d+\z/))
-              yield Issue.new(
-                id: category,
-                path: path,
-                location: no_line_number ? nil : Location.new(start_line: line),
-                message: message.strip,
-                object: {
-                  confidence: confidence,
-                },
-                schema: Schema.issue,
-              )
-            else
-              raise "Unexpected message format: #{issue_line.inspect}"
-            end
+          result = failure.text or raise "Required result: #{failure.inspect}"
+          result.scan(issue_pattern) do |match|
+            line, message, category, confidence = match
+            no_line_number = (line == "0" || !line.match?(/\A\d+\z/))
+            issues << Issue.new(
+              id: category,
+              path: path,
+              location: no_line_number ? nil : Location.new(start_line: line),
+              message: message.strip,
+              object: {
+                confidence: confidence,
+              },
+              schema: Schema.issue,
+            )
           end
         end
       end
+
+      issues
     end
   end
 end
