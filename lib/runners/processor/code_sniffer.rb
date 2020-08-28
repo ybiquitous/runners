0e857a41a24680b8cbefaba2b97c7d93db2f4c21
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-28 09:00:11 +0900

    Refactor `Runners::Processor::CodeSniffer` class (#1432)
    Extract from #1215

--- lib/runners/processor/code_sniffer.rb
+++ lib/runners/processor/code_sniffer.rb
@@ -2,8 +2,9 @@ module Runners
   class Processor::CodeSniffer < Processor
     include PHP
 
-    Schema = StrongJSON.new do
-      let :runner_config, Schema::BaseConfig.base.update_fields { |fields|
+    Schema = _ = StrongJSON.new do
+      # @type self: SchemaClass
+      let :runner_config, Runners::Schema::BaseConfig.base.update_fields { |fields|
         fields.merge!({
                         version: enum?(string, numeric),
                         dir: string?,
@@ -31,58 +32,89 @@ module Runners
 
     register_config_schema(name: :code_sniffer, schema: Schema.runner_config)
 
+    def analyzer_bin
+      "phpcs"
+    end
+
     def setup
       add_warning_if_deprecated_options
       yield
     end
 
     def analyze(changes)
-      run_analyzer additional_options, directory
+      capture3!(
+        analyzer_bin,
+        "--report=json",
+        "--report-json=#{report_file}",
+        "-q", # Enable quiet mode. See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Advanced-Usage#quieting-output
+        "--runtime-set", "ignore_errors_on_exit", "1", # See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Configuration-Options#ignoring-errors-when-generating-the-exit-code
+        "--runtime-set", "ignore_warnings_on_exit", "1", # See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Configuration-Options#ignoring-warnings-when-generating-the-exit-code
+        *additional_options,
+        directory,
+      )
+
+      issues = []
+
+      read_report_json.fetch(:files).each do |file, suggests|
+        path = relative_path(file.to_s)
+
+        suggests.fetch(:messages).each do |suggest|
+          issues << Issue.new(
+            path: path,
+            location: Location.new(start_line: suggest[:line], start_column: suggest[:column]),
+            id: suggest[:source],
+            message: suggest[:message],
+            object: {
+              type: suggest[:type],
+              severity: suggest[:severity],
+              fixable: suggest[:fixable],
+            },
+            schema: Schema.issue,
+          )
+        end
+      end
+
+      issues.uniq! { |issue| [issue.path, issue.location, issue.id, issue.message] }
+
+      Results::Success.new(guid: guid, analyzer: analyzer, issues: issues)
     end
 
     private
 
-    def analyzer_bin
-      "phpcs"
-    end
-
     def additional_options
-      # If a repository doesn't have `sider.yml`, use default configuration with `default_sideci_options` method.
       if config_linter.empty?
-        default_sideci_options[:options].map do |k, v|
-          "--#{k}=#{v}"
-        end
+        (_ = default_options.fetch(:options)).map { |k, v| "--#{k}=#{v}" }
       else
-        [standard_option, extensions_option, encoding_option, ignore_option].compact
+        [standard_option, extensions_option, *encoding_option, *ignore_option]
       end
     end
 
     def standard_option
-      standard = config_linter[:standard] || config_linter.dig(:options, :standard) || default_sideci_options.dig(:options, :standard)
+      standard = config_linter[:standard] || config_linter.dig(:options, :standard) || default_options.dig(:options, :standard)
       "--standard=#{standard}"
     end
 
     def extensions_option
-      extensions = config_linter[:extensions] || config_linter.dig(:options, :extensions) || default_sideci_options.dig(:options, :extensions)
+      extensions = config_linter[:extensions] || config_linter.dig(:options, :extensions) || default_options.dig(:options, :extensions)
       "--extensions=#{extensions}"
     end
 
     def encoding_option
       encoding = config_linter[:encoding] || config_linter.dig(:options, :encoding)
-      "--encoding=#{encoding}" if encoding
+      encoding ? ["--encoding=#{encoding}"] : []
     end
 
     def ignore_option
       ignore = config_linter[:ignore] || config_linter.dig(:options, :ignore)
-      "--ignore=#{ignore}" if ignore
+      ignore ? ["--ignore=#{ignore}"] : []
     end
 
     def directory
-      config_linter[:dir] || config_linter.dig(:options, :dir) || default_sideci_options[:dir]
+      config_linter[:dir] || config_linter.dig(:options, :dir) || default_options.fetch(:dir)
     end
 
-    def default_sideci_options
-      @default_sideci_options ||=
+    def default_options
+      @default_options ||=
         case php_framework
         when :CakePHP
           {
@@ -112,50 +144,9 @@ module Runners
     end
 
     def php_framework
-      @php_framework ||= {
-        CakePHP: 'lib/Cake/Core/CakePlugin.php',
-        Symfony: 'app/SymfonyRequirements.php',
-      }.find do |framework, file|
-        break framework if (current_dir / file).exist?
-      end
-    end
-
-    def run_analyzer(options, target)
-      capture3!(
-        analyzer_bin,
-        "--report=json",
-        "--report-json=#{report_file}",
-        "-q", # Enable quiet mode. See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Advanced-Usage#quieting-output
-        "--runtime-set", "ignore_errors_on_exit", "1", # See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Configuration-Options#ignoring-errors-when-generating-the-exit-code
-        "--runtime-set", "ignore_warnings_on_exit", "1", # See https://github.com/squizlabs/PHP_CodeSniffer/wiki/Configuration-Options#ignoring-warnings-when-generating-the-exit-code
-        *options,
-        target
-      )
-
-      Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
-        issues = []
-
-        read_report_json[:files].each do |path, suggests|
-          suggests[:messages].each do |suggest|
-            issues << Issue.new(
-              path: relative_path(path.to_s),
-              location: Location.new(start_line: suggest[:line], start_column: suggest[:column]),
-              id: suggest[:source],
-              message: suggest[:message],
-              object: {
-                type: suggest[:type],
-                severity: suggest[:severity],
-                fixable: suggest[:fixable],
-              },
-              schema: Schema.issue,
-            )
-          end
-        end
-
-        issues.uniq { |issue| [issue.path, issue.location, issue.id, issue.message] }.each do |issue|
-          result.add_issue issue
-        end
-      end
+      return :CakePHP if File.exist? 'lib/Cake/Core/CakePlugin.php'
+      return :Symfony if File.exist? 'app/SymfonyRequirements.php'
+      nil
     end
   end
 end
