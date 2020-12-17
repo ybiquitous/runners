4c69184834d811fbd329041d80fbd20498a0cda8
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-12-17 21:59:23 +0900

    Fix RBS for `Runners::Processor::Stylelint` (#1791)

--- lib/runners/processor/stylelint.rb
+++ lib/runners/processor/stylelint.rb
@@ -2,7 +2,9 @@ module Runners
   class Processor::Stylelint < Processor
     include Nodejs
 
-    Schema = StrongJSON.new do
+    Schema = _ = StrongJSON.new do
+      # @type self: SchemaClass
+
       let :runner_config, Schema::BaseConfig.npm.update_fields { |fields|
         fields.merge!({
                         config: string?,
@@ -38,6 +40,9 @@ module Runners
 
     DEFAULT_TARGET_FILES = "*.{css,less,sass,scss,sss}".freeze
     DEFAULT_GLOB = "**/#{DEFAULT_TARGET_FILES}".freeze
+    DEFAULT_CONFIG_FILE = (Pathname(Dir.home) / 'sider_recommended_config.yaml').to_path.freeze
+    DEFAULT_CONFIG_FILE_OLD = (Pathname(Dir.home) / 'sider_recommended_config.old.yaml').to_path.freeze
+    DEFAULT_IGNORE_FILE = (Pathname(Dir.home) / 'sider_recommended_stylelintignore').to_path.freeze
 
     def setup
       add_warning_if_deprecated_options
@@ -59,9 +64,46 @@ module Runners
     def analyze(changes)
       delete_unchanged_files(changes, only: [DEFAULT_TARGET_FILES])
 
-      additional_options = [stylelint_config, syntax, ignore_path, ignore_disables,
-                            report_needless_disables, quiet].flatten.compact
-      run_analyzer(glob, additional_options)
+      default_options = ['--formatter=json', '--no-color']
+
+      # stylelint v10.0.0 throws an error if glob matches no files.
+      # @see https://github.com/stylelint/stylelint/releases/tag/10.0.0
+      # This behavior is inconvenient when deleting unchanged files
+      #
+      # ---
+      #
+      # The `--allow-empty-input` option breaks with v10.0.0. Fixed with v10.0.1.
+      # @see https://github.com/stylelint/stylelint/releases/tag/10.0.1
+      # @see https://github.com/stylelint/stylelint/pull/4029
+      if Gem::Version.create(analyzer_version) >= Gem::Version.create("10.0.1")
+        default_options << '--allow-empty-input'
+      end
+
+      stdout, stderr, status = capture3(
+        nodejs_analyzer_bin,
+        *default_options,
+        *stylelint_config,
+        *syntax,
+        *ignore_path,
+        *ignore_disables,
+        *report_needless_disables,
+        *quiet,
+        glob
+      )
+
+      # https://github.com/stylelint/stylelint/blob/master/docs/user-guide/cli.md#exit-codes
+      # 0 => No Issues
+      # 2 => Issues exist
+      # Others => Something wrong, target files don't exist, ...
+      unless [0, 2].include?(status.exitstatus)
+        return Results::Failure.new(guid: guid, message: [stdout, stderr].join("\n"), analyzer: analyzer)
+      end
+
+      Results::Success.new(guid: guid, analyzer: analyzer, issues: parse_result(stdout)).tap do |result|
+        @warning_set&.sort&.each do |warning|
+          add_warning(warning, file: config_file_path&.to_path)
+        end
+      end
     end
 
     private
@@ -71,36 +113,33 @@ module Runners
     end
 
     def stylelint_config
-      stylelintrc = config_linter[:config] || config_linter.dig(:options, :config)
-      "--config=#{stylelintrc}" if stylelintrc
+      config = config_linter[:config] || config_linter.dig(:options, :config)
+      config ? ["--config=#{config}"] : []
     end
 
     def syntax
       syntax = config_linter[:syntax] || config_linter.dig(:options, :syntax)
-      "--syntax=#{syntax}" if syntax
+      syntax ? ["--syntax=#{syntax}"] : []
     end
 
     def ignore_path
-      ignore_path = config_linter[:'ignore-path'] || config_linter.dig(:options, :'ignore-path')
-      "--ignore-path=#{ignore_path}" if ignore_path
+      path = config_linter[:'ignore-path'] || config_linter.dig(:options, :'ignore-path')
+      path ? ["--ignore-path=#{path}"] : []
     end
 
     def ignore_disables
-      ignore_disables = config_linter[:'ignore-disables'] || config_linter.dig(:options, :'ignore-disables')
-      "--ignore-disables" if ignore_disables
+      (config_linter[:'ignore-disables'] || config_linter.dig(:options, :'ignore-disables')) ? ["--ignore-disables"] : []
     end
 
     def report_needless_disables
       rd = config_linter[:'report-needless-disables'] || config_linter.dig(:options, :'report-needless-disables')
-      "--report-needless-disables" if rd
+      rd ? ["--report-needless-disables"] : []
     end
 
     def quiet
-      quiet = config_linter[:quiet] || config_linter.dig(:options, :quiet)
-      "--quiet" if quiet
+      (config_linter[:quiet] || config_linter.dig(:options, :quiet)) ? ["--quiet"] : []
     end
 
-    # @param stdout [String]
     def parse_result(stdout)
       JSON.parse(stdout, symbolize_names: true).flat_map do |file|
         check_warning(file[:deprecations])
@@ -115,7 +154,7 @@ module Runners
             location: line ? Location.new(start_line: line, start_column: warning[:column]) : nil, # NOTE: There is a rare case that stylelint doesn't output the `line`.
             id: id,
             message: normalize_message(id, warning[:text]),
-            links: [rule_doc_urls[id]].compact,
+            links: Array(rule_doc_urls[id]),
             object: {
               severity: warning[:severity],
             },
@@ -154,19 +193,19 @@ module Runners
 
       # NOTE: `stylelint-config-recommended@2` does not work with `stylelint@11`.
       src = if Gem::Version.create(analyzer_version) >= Gem::Version.create("11.0.0")
-              (Pathname(Dir.home) / 'sider_recommended_config.yaml').realpath
+              DEFAULT_CONFIG_FILE
             else
-              (Pathname(Dir.home) / 'sider_recommended_config.old.yaml').realpath
+              DEFAULT_CONFIG_FILE_OLD
             end
-      dst = current_dir.join('.stylelintrc.yaml')
-      FileUtils.copy(src, dst)
+
+      FileUtils.copy_file(src, '.stylelintrc.yaml')
     end
 
     def prepare_ignore_file
-      ignore_file_path = current_dir.join('.stylelintignore')
-      return if ignore_file_path.file?
-      src = (Pathname(Dir.home) / 'sider_recommended_stylelintignore').realpath
-      FileUtils.copy(src, ignore_file_path)
+      ignore_file = '.stylelintignore'
+      unless File.exist? ignore_file
+        FileUtils.copy_file(DEFAULT_IGNORE_FILE, ignore_file)
+      end
     end
 
     # returns available config file path. If the file doesn't exist, it returns nil.
@@ -186,7 +225,7 @@ module Runners
           .stylelintrc.yml
           stylelint.config.js
         ]
-        candidates.map { |filename| current_dir / filename }.find(&:exist?)
+        candidates.find { |filename| File.exist?(filename) }&.then { |filename| current_dir / filename }
       end
     end
 
@@ -195,50 +234,10 @@ module Runners
     # Without this method, if we write a warning when the "deprecations" appear,
     # the same number of duplicated warnings as files will appear.
     # We use `Set` to avoid it.
-    #
-    # @param deprecations [Array<Hash>]
     def check_warning(deprecations)
       @warning_set ||= Set.new
       deprecations.each do |dep|
         @warning_set << "#{dep[:text]} #{dep[:reference]}"
-      end
-    end
-
-    def run_analyzer(glob, additional_options)
-      default_options = ['--formatter=json', '--no-color']
-      # stylelint v10.0.0 throws an error if glob matches no files.
-      # @see https://github.com/stylelint/stylelint/releases/tag/10.0.0
-      # This behavior is inconvenient when deleting unchanged files
-      #
-      # ---
-      #
-      # The `--allow-empty-input` option breaks with v10.0.0. Fixed with v10.0.1.
-      # @see https://github.com/stylelint/stylelint/releases/tag/10.0.1
-      # @see https://github.com/stylelint/stylelint/pull/4029
-      if Gem::Version.new(analyzer_version) >= Gem::Version.new("10.0.1")
-        default_options << '--allow-empty-input'
-      end
-
-      stdout, stderr, status = capture3(
-        nodejs_analyzer_bin,
-        *default_options,
-        *additional_options,
-        glob
-      )
-
-      # https://github.com/stylelint/stylelint/blob/master/docs/user-guide/cli.md#exit-codes
-      # 0 => No Issues
-      # 2 => Issues exist
-      # Others => Something wrong, target files don't exist, ...
-      unless [0, 2].include?(status.exitstatus)
-        return Results::Failure.new(guid: guid, message: [stdout, stderr].join("\n"), analyzer: analyzer)
-      end
-
-      Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
-        parse_result(stdout).each { |v| result.add_issue(v) }
-        @warning_set&.sort&.each do |warning|
-          add_warning(warning, file: config_file_path&.to_s)
-        end
       end
     end
   end
