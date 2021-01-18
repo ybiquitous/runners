27562a5d1e66b9d4fdcebac1ad865d4a88723f19
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-01-18 10:44:37 +0900

    Refactor smoke test runner (#1944)

--- tasks/docker/smoke.rake
+++ tasks/docker/smoke.rake
@@ -1,9 +1,15 @@
 require_relative "common"
+require_relative "../../test/smoke"
 
 namespace :docker do
   desc 'Run smoke test on Docker'
   task :smoke do
     include DockerTaskCommon
-    sh "bin/runners_smoke", image_name, "test/smokes/#{analyzer}/expectations.rb"
+
+    Runners::Testing::Smoke.new(
+      docker_image: image_name,
+      expectations_path: "test/smokes/#{analyzer}/expectations.rb",
+      analyzer: analyzer,
+    ).run
   end
 end
--- test/smoke.rb
+++ test/smoke.rb
@@ -38,31 +38,21 @@ module Runners
         end
       end
 
-      attr_reader :argv
+      attr_reader :docker_image, :expectations_path, :analyzer
 
-      def initialize(argv)
-        @argv = argv
-      end
-
-      def docker_image
-        argv[0]
-      end
-
-      def entrypoint
-        Pathname(argv[0])
-      end
-
-      def expectations
-        Pathname(argv[1])
+      def initialize(docker_image:, expectations_path:, analyzer:)
+        @docker_image = docker_image
+        @expectations_path = Pathname(expectations_path)
+        @analyzer = analyzer
       end
 
       def run
         start = Time.now
 
-        load expectations.to_path
+        load expectations_path.to_path
 
         jobs = ENV["JOBS"] ? Integer(ENV["JOBS"]) : nil
-        "Running #{Rainbow(self.class.tests.size.to_s).bright} smoke tests".tap do |msg|
+        "[#{Rainbow(analyzer).blue.bright.underline}] Running #{Rainbow(self.class.tests.size.to_s).bright} smoke tests".tap do |msg|
           msg << " with #{Rainbow(jobs.to_s).bright} jobs" if jobs
           puts "#{msg}..."
         end
@@ -76,7 +66,7 @@ module Runners
           print out.string
           duration_per_test = (Time.now - start_per_test).round(1)
           puts "#{marks[result]} #{Rainbow(params.name).bright.underline}" + \
-               Rainbow(" (#{duration_per_test}s)").darkgray.to_s
+               Rainbow(" (#{duration_per_test} seconds)").darkgray.to_s
           [result, params.name]
         }
 
@@ -112,7 +102,7 @@ module Runners
         command_output, _ = Dir.mktmpdir do |dir|
           repo_args = {
             workdir: Pathname(dir).realpath,
-            smoke_target: expectations.parent.join(params.name).realpath,
+            smoke_target: expectations_path.parent.join(params.name).realpath,
             out: out,
           }
           if params.use_git_metadata
