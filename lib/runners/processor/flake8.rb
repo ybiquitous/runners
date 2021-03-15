3f2678819641993e040952417ab95d83555bafbd
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-03-15 12:31:41 +0900

    [Flake8] Simplify `Runners::Processor::Flake8` a bit (#2180)

--- lib/runners/processor/flake8.rb
+++ lib/runners/processor/flake8.rb
@@ -27,7 +27,8 @@ module Runners
     OUTPUT_PATTERN = /^([^:]+):::([^:]+):::(\d+):::(\d+):::(.+)$/.freeze
 
     DEFAULT_TARGET = ".".freeze
-    IGNORED_CONFIG_PATH = (Pathname(Dir.home) / '.config/ignored-config.ini').to_path.freeze
+    DEFAULT_CONFIG_PATH = (Pathname(Dir.home) / '.config' / 'flake8').to_path.freeze
+    DEFAULT_IGNORED_CONFIG_PATH = (Pathname(Dir.home) / '.config' / 'ignored-config.ini').to_path.freeze
 
     def self.config_example
       <<~'YAML'
@@ -57,29 +58,29 @@ module Runners
         "--exit-zero",
         "--output-file", report_file,
         "--format", OUTPUT_FORMAT,
-        "--append-config", IGNORED_CONFIG_PATH,
+        "--append-config", DEFAULT_IGNORED_CONFIG_PATH,
         "--jobs", parallel ? "auto" : "1",
         *(config_linter[:config]&.then { |c| ["--config", c] }),
         *Array(config_linter[:target] || DEFAULT_TARGET),
       )
       output = read_report_file
 
-      Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
-        next if output.empty?
-        parse_result(output) { |issue| result.add_issue(issue) }
-      end
+      Results::Success.new(guid: guid, analyzer: analyzer, issues: parse_result(output))
     end
 
     private
 
     def prepare_config
-      default_config = (Pathname(Dir.home) / '.config/flake8').realpath
-      return default_config.delete if (current_dir / '.flake8').exist?
-      configs = %w[setup.cfg tox.ini].select do |v|
-        path = (current_dir + v)
-        path.exist? && path.read.match(/^\[flake8\]$/m)
+      # @see https://flake8.pycqa.org/en/latest/user/configuration.html
+      if (current_dir / '.flake8').exist?
+        File.delete DEFAULT_CONFIG_PATH
       end
-      return default_config.delete unless configs.empty?
+
+      current_dir.glob('{setup.cfg,tox.ini}').each do |config_file|
+        if config_file.exist? && config_file.read.match?(/^\[flake8\]$/m)
+          File.delete DEFAULT_CONFIG_PATH
+        end
+      end
     end
 
     def prepare_plugins
@@ -87,15 +88,17 @@ module Runners
     end
 
     def parse_result(output)
+      issues = []
       output.scan(OUTPUT_PATTERN) do |match|
         id, path, line, column, message = match
-        yield Issue.new(
+        issues << Issue.new(
           path: relative_path(path),
           location: Location.new(start_line: line, start_column: column),
           id: id,
           message: message,
         )
       end
+      issues
     end
   end
 end
