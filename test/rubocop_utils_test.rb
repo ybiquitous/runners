5af9b0e4d315c902947277c3984531a49ed26b99
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-29 18:19:11 +0900

    Fix RuboCopUtilsTest (#2487)
    Now <https://github.com/rubocop/rubocop-rake> is public.

--- test/rubocop_utils_test.rb
+++ test/rubocop_utils_test.rb
@@ -76,8 +76,8 @@ class RuboCopUtilsTest < Minitest::Test
       ], "Jekyll/NoPAllowed"
       assert_links.call %w[
         https://www.rubydoc.info/gems/rubocop-rake/RuboCop/Cop/Rake/Desc
-        https://github.com/rubocop-hq/rubocop-rake
-      ], "Rake/Desc", additional_statuses: [301] # TODO: Remove `additional_statuses`
+        https://github.com/rubocop/rubocop-rake
+      ], "Rake/Desc"
       assert_links.call %w[
         https://www.rubydoc.info/gems/rubocop-rubycw/RuboCop/Cop/Rubycw/Rubycw
         https://github.com/rubocop/rubocop-rubycw
